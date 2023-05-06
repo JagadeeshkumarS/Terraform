6fdd6224df3323b90a7462ab4cafb057b6dbbc90
JagadeeshkumarS <129980043+JagadeeshkumarS@users.noreply.github.com>
2023-05-06 11:43:48 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,13 @@
 #Declaring AWS Provider and credentials
 provider "aws" {
-  region     = "us-east-1"
+  region     = var.region
   }
 #######################################################
 #Creating Production VPC with CIDR: 172.16.0.0/16
 resource "aws_vpc" "vpc" {
     cidr_block = "172.16.0.0/16" 
     tags = {
-        Name = "J VPC"
+        Name = "JJJ VPC"
     }
 }
 output "vpcid" {
